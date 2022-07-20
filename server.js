3a576b4504babb6fd2c6d371e5ee8b726ad5a78b
shafin_shant0 <shafinshanto9@gmail.com>
2022-07-20 14:31:57 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => res.send('hellow world'))
+app.listen(port,()=>console.log(`Node js Server Running At ${port}`))
