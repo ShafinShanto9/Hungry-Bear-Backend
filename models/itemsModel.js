be095f00ea1547c43ff6b4a245d9a8996f64bef3
shafin_shant0 <shafinshanto9@gmail.com>
2022-07-26 16:51:25 +0600

    post data routes create

--- models/itemsModel.js
+++ models/itemsModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 const itemsSchema = mongoose.Schema({
     name: {type:String, required:true },
-    image: {type:String, required:true },
+    Image: {type:String, required:true },
     price: {type:Number, required:true },
     category: {type:String, required:true }
-})
+}, {timestamps:true})
 
 const itemsModel = mongoose.model('items', itemsSchema)
 module.exports = itemsModel
--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -11,4 +11,15 @@ router.get("/get-all-items", async(req, res) => {
     }
 })  
 
+router.post("/add-item", async(req, res) => {
+    try {
+        const newItem = new itemModel(req.body)
+        await newItem.save()
+        res.send('Item Added Successfully')
+    } catch (error) {
+        res.status(400).json(error)
+        console.log(error);
+    }
+}) 
+
 module.exports = router
