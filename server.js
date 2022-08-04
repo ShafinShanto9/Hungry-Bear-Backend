f6fb912051dbca08bda261bd32fb320f2ae7387b
shafin_shant0 <shafinshanto9@gmail.com>
2022-08-04 16:56:10 +0600

    issue fixed

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const itemsRoute = require('./routes/itemsRoute')
 const userRoute = require('./routes/userRoute')
 const billsRoute = require('./routes/billsRoute') 
 require("dotenv").config();
-
+const cors = require("cors");
 
 const app = express()
 app.use(express.json())
-const path = require('path')
-const port = process.env.PORT || 5000
+app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/',express.static('../client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
-  })
-}
+const port = process.env.PORT || 5000
 
 
 app.use('/api/items/', itemsRoute)
